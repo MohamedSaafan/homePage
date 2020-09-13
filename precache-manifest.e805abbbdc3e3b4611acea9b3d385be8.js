9a67b47dc4bf11709c927218303624fdd97b3428
MohamedSaffan <201827254@std.sci.cu.edu.eg>
2020-09-13 08:25:51 +0200

    Updates

---
+++ precache-manifest.e805abbbdc3e3b4611acea9b3d385be8.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "51592c936a34823355011d3ed3cb0c2a",
+    "url": "/homePage/index.html"
+  },
+  {
+    "revision": "a6b68e6718131ef18921",
+    "url": "/homePage/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fdfc518e8c886d08cf43",
+    "url": "/homePage/static/js/2.5f4523cd.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/homePage/static/js/2.5f4523cd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6b68e6718131ef18921",
+    "url": "/homePage/static/js/main.989a4ed7.chunk.js"
+  },
+  {
+    "revision": "9ac927b9fcc062f73a5f",
+    "url": "/homePage/static/js/runtime-main.00957640.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/homePage/static/media/logo.5d5d9eef.svg"
+  }
+]);
